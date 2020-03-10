72f2852461c70493f98379187d7bf709c408b9d5
Bruno Almeida <brunoslalmeida@gmail.com>
2020-03-10 17:27:23 -0300

    adding wp download

--- run.sh
+++ run.sh
@@ -5,8 +5,22 @@ then
   exit -1
 fi
 
+pwd=$(pwd)
 cwd=$(pwd)/site/$1
 name=wordpress_$1
+
+if ! test -d "$pwd/wordpress"; 
+then
+  wget https://wordpress.org/latest.tar.gz
+  tar -zxvf latest.tar.gz
+  rm -rf latest.tar.gz
+fi
+
+if ! test -d "$cwd";
+then 
+  mkdir -p $cwd
+  cp -R $pwd/wordpress $cwd
+fi
 
 docker stop $name
 docker rm nginx $name
@@ -16,7 +30,5 @@ docker run -d \
   --hostname $name \
   --restart=always \
   --network internal \
-  -v $cwd/themes:/var/www/html/wp-content/themes \
-  -v $cwd/plugins:/var/www/html/wp-content/plugins \
-  -v $cwd/uploads:/var/www/html/wp-content/uploads \
+  -v $cwd/wordpress:/var/www/html \
   wordpress
